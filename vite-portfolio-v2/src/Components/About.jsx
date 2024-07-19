99b4a76a37255278f12659c8148bc949a8e4fbb9
T-Godwins <gtuyishime@gmail.com>
2024-07-19 04:17:49 -0400

    good headway

--- vite-portfolio-v2/src/Components/About.jsx
+++ vite-portfolio-v2/src/Components/About.jsx
@@ -1,16 +1,31 @@
 import './About.css'
 import headshot from '../assets/Headshot.jpg'
+const python = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg";
+const cplus = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg";
 
 function About(){
     return(
+        <>
         <div className="about">
-            <img className="card-image" src={headshot} alt="Headshot"></img>
-            <h2 className="card-title">Learn More About Me</h2>
-            <p className="card-text">I am currently a Data and Information Fellow at Wofford College's Career Center, where I focus on automating and improving data management processes. With a solid foundation in Python and a growing expertise in Tableau, I leverage AI to streamline tasks and enhance data-driven insights. I am also a
+            <img src={headshot} alt="Headshot"></img>
+            <h2>Learn More About Me</h2>
+            <p>I am currently a Data and Information Fellow at Wofford College's Career Center, where I focus on automating and improving data management processes. With a solid foundation in Python and a growing expertise in Tableau, I leverage AI to streamline tasks and enhance data-driven insights. I am also a
             Software Engineering Fellow at Headstarter where I am embarking on building 5 AI projects in 5 weeks.
             </p>
-
         </div>
+        <div className="skills">
+            <ul>
+                          
+                <li>Programming Languages: Python, R, C++</li>
+                <img className="python" src={python} alt="python logo"></img>
+                {/* <img className="cplus" src={cplus} alt="C++ logo"></img> */}
+
+                <li>Data Analysis Tools: SQL, Tableau, Microsoft Excel</li>
+                <li>Other Tools: Microsoft PowerPoint, Adobe Photoshop</li>
+                <li>Certifications: Currently pursuing AWS Cloud Practitioner certification</li>
+            </ul>
+        </div>
+        </>
     );
 }
 
--- vite-portfolio-v2/src/Components/Navbar.jsx
+++ vite-portfolio-v2/src/Components/Navbar.jsx
@@ -2,20 +2,23 @@ import './Navbar.css'
 import linkedinLogo from '../assets/linkedin.svg'
 import gmailLogo from '../assets/envelope.svg'
 import gitLogo from '../assets/github.svg'
-
+const linkedin = 'https://linkedin.com/in/godwins-tuyishime'
+const github = 'https://github.com/T-Godwins'
 
 
 function Navbar(){
     return(
         <>
         <div className='navbar'>
+            <div className='left'>
                 <button><a href='#' alt=''>About Me</a></button>
                 <button><a href='#' alt='Experience'>Experience</a> </button>
-               
-                <button><a href='#' alt='linked in'><img src={linkedinLogo} className='linkedin' alt='linkedin'></img></a></button>
-                <button><a href='#' alt='linked in'><img src={gmailLogo} className='gmail' alt='gmail'></img></a></button>
-                <button><a href='#' alt='linked in'><img src={gitLogo} className='github' alt='github'></img></a></button>
-            
+                </div>
+            <div className='right'>
+                <button><a href={linkedin} alt='linked in'><img src={linkedinLogo} className='linkedin' target="_blank" alt='linkedin'></img></a></button>
+                <button><a href='#' alt='gmail'><img src={gmailLogo} className='gmail' alt='gmail'></img></a></button>
+                <button><a href={github} alt='github'><img src={gitLogo} className='github' alt='github'></img></a></button>
+            </div>
         </div>
         </>
     );
